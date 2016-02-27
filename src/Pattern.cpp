884639a6f9b0a279203393a6a0f519c5eae3ac00
Mikey Appuhn <mike.appuhn@gmail.com>
2016-02-27 02:13:28 -0500

    Added a "Pattern" class, an abstract class for colors and shape patterns in our lovely grid.

---
+++ src/Pattern.cpp
@@ -0,0 +1,10 @@
+//
+//  Pattern.cpp
+//  shapeMachine
+//
+//  Created by m on 2/27/16.
+//
+//
+
+#include "Pattern.hpp"
+
---
+++ src/Pattern.hpp
@@ -0,0 +1,21 @@
+//
+//  Pattern.hpp
+//  shapeMachine
+//
+//  Created by m on 2/27/16.
+//
+//
+
+#ifndef Pattern_hpp
+#define Pattern_hpp
+
+#include <stdio.h>
+
+class Pattern {
+public:
+    Pattern();
+    
+};
+
+
+#endif /* Pattern_hpp */
